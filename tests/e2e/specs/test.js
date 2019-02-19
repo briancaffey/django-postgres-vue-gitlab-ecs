172a5cd1dc808db3bac592872d567ea5b4e2af72
briancaffey <briancaffey2010@gmail.com>
2019-02-19 01:10:56 -0500

    added e2e test with cypress

--- tests/e2e/specs/test.js
+++ tests/e2e/specs/test.js
@@ -3,6 +3,11 @@
 describe('My First Test', () => {
   it('Visits the app root url', () => {
     cy.visit('/')
-    cy.contains('h1', 'Welcome to Your Vue.js App')
+    cy.contains('h1', 'Welcome to Brian Caffey\'s Vue.js App')
+  });
+
+  it('Visits the About page', () => {
+    cy.visit('/about')
+    cy.contains('h1', 'This is an about page');
   })
 })
