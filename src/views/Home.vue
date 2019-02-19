55ea0353dfc4bc7b32b4e866bfd4222a446d2be3
briancaffey <briancaffey2010@gmail.com>
2019-02-18 20:51:01 -0500

    updated gitlab-ci and changed home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Welcome to Brian Caffey's Vue.js App"/>
   </div>
 </template>
 
