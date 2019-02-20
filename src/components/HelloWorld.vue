dae9f1bc79fcd2b05b4fc1798852f3155f2a6a42
briancaffey <briancaffey2010@gmail.com>
2019-02-19 21:49:36 -0500

    testing env var in build

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
+    <p>Commit ref: {{ commit }}</p>
     <p>
       For a guide and recipes on how to configure / customize this project,<br>
       check out the
@@ -38,6 +39,11 @@ export default {
   name: 'HelloWorld',
   props: {
     msg: String
+  },
+  data(){
+    return {
+      commit: process.env.CI_COMMIT_SHORT_SHA || 'some commit'
+    }
   }
 }
 </script>
