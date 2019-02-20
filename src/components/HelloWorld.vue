d6bfff3b0cdd59dbcbd073481f1d402dc67681cb
briancaffey <briancaffey2010@gmail.com>
2019-02-19 23:31:35 -0500

    added cloudfront invalidation to build stage

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,6 +2,7 @@
   <div class="hello">
     <h1>{{ msg }}</h1>
     <p>Commit ref: {{ commit }}</p>
+    <p>Using cache invalidation</p>
     <p>
       For a guide and recipes on how to configure / customize this project,<br>
       check out the
