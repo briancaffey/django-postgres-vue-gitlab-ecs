f76d37fb34b0cc3fde4a954fa43ce5571da9c9f9
briancaffey <briancaffey2010@gmail.com>
2019-03-07 01:12:10 -0500

    added infrastructure deployment to gitlab ci, updated backend container

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('/', include('core.urls',)),
+    path('', include('core.urls',)),
     path('admin/', admin.site.urls),
     path('api/', include('core.urls'))
 ]
