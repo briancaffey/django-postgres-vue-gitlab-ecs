8fe437561b7b7da4f205c5bb398dea83885874c9
briancaffey <briancaffey2010@gmail.com>
2019-03-07 15:07:38 -0500

    added command to create defalt user

---
+++ backend/core/management/commands/create_default_user.py
@@ -0,0 +1,25 @@
+from django.contrib.auth import get_user_model
+from django.core.management.base import BaseCommand
+
+
+class Command(BaseCommand):
+    help = 'Creates a default superuser for local development'
+
+    def handle(self, *args, **options):
+        User = get_user_model()
+        if not User.objects.all():
+            print("Creating default user")
+            User.objects.create_superuser(
+                'admin',
+                'admin@company.com',
+                'password'
+            )
+            print(
+                """
+                Default user created:
+                username: 'admin'
+                email: 'admin@company.com'
+                password: 'password'
+                """)
+        else:
+            print("Not creating default user")
