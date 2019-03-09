092c87c75fb637e77dfacfa98bed7f9130f6cd85
briancaffey <briancaffey2010@gmail.com>
2019-03-09 17:00:08 -0500

    fixed allowed hosts in django settings

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'secret-key')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', True)
 
-ALLOWED_HOSTS = ['*.briancaffey.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['backend.briancaffey.com', 'localhost', '127.0.0.1']
 
 CORS_ORIGIN_ALLOW_ALL = True
 
