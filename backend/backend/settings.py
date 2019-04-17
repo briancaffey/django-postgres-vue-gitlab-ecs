ca91cdec6d1fa5dc8b61e40bfeb19f468fa5c875
Brian Caffey <briancaffey2010@gmail.com>
2019-04-17 09:27:18 -0400

    added new assets service with correct bucket name, renamed backend service

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -161,7 +161,7 @@ else:
     AWS_DEFAULT_ACL = None
     AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
     AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
-    AWS_STORAGE_BUCKET_NAME = f"{os.environ['APP_URL']}-assets"
+    AWS_STORAGE_BUCKET_NAME = f"{os.environ['STACK_NAME']}-assets"
     AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com"
     AWS_S3_OBJECT_PARAMETERS = {
         'CacheControl': 'max-age=86400',
