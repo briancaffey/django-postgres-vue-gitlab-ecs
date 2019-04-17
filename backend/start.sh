398bf2b097ae0522608477f12893c7336fcb57a5
Brian Caffey <briancaffey2010@gmail.com>
2019-04-16 22:44:18 -0400

    removed files

--- backend/start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-python3 manage.py collectstatic --no-input
-python3 manage.py makemigrations
-python3 manage.py migrate --no-input
-python3 manage.py create_default_user
-python3 manage.py runserver 0.0.0.0:8000
