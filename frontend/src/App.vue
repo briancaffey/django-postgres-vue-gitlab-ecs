bb36ee2fb28cd3b535d32a7d41455d47d4a980d9
Brian Caffey <briancaffey2010@gmail.com>
2019-04-25 00:09:50 -0400

    added fade transitions

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -7,7 +7,11 @@
       <span v-if="isAuthenticated"><router-link to="/sockets">Sockets</router-link> | </span>
       <span><a v-if="isAuthenticated" href="#logout" @click="logout">Logout</a></span>
     </div>
-    <router-view/>
+        <transition
+          name="fade"
+          mode="out-in">
+          <router-view :key="$route.path"/>
+        </transition>
   </div>
 </template>
 
@@ -58,6 +62,19 @@ export default {
 </script>
 
 <style lang="scss">
+  .fade-enter-active,
+  .fade-leave-active {
+    transition-duration: 0.15s;
+    transition-property: opacity;
+    transition-timing-function: ease;
+  }
+
+  .fade-enter,
+  .fade-leave-active {
+    opacity: 0
+  }
+
+
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
