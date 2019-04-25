3d2df363b26da8bac847131225754bf276e8c2c1
Brian Caffey <briancaffey2010@gmail.com>
2019-04-25 01:33:54 -0400

    finished basic chatroom

--- frontend/src/views/Sockets.vue
+++ frontend/src/views/Sockets.vue
@@ -1,11 +1,15 @@
 <template>
   <div>
     <h1>
-    Sockets
+    WebSocket Chat Room
     </h1>
-    {{ isConnected }}
-    <input placeholder="type message" v-model="message">
-    <button @click="sendMessage">Click me</button>
+    <div ref="chat-area" class="chat-area">
+      <p v-for="(m, i) in messages" :key="i">
+        {{ m }}
+      </p>
+    </div>
+    <input ref="message" placeholder="type your message..." v-model="message">
+    <button @click="sendMessage(message)">Send</button>
   </div>
 </template>
 
@@ -14,25 +18,48 @@
     data() {
       return {
         message: '',
-        isConnected: false,
+        messages: []
       }
     },
     methods: {
       sendMessage(message){
-        console.log("clicking")
-        this.$socket.send(JSON.stringify({"message":this.message}))
+        var outboundMessage = message;
+        this.message = '';
+        this.$socket.send(JSON.stringify({"message":outboundMessage}));
+        this.$refs.message.focus();
+      },
+      scrollToBottom() {
+        const messageDisplay = this.$refs["chat-area"];
+        console.log(messageDisplay.scrollHeight);
+        setTimeout(() => {messageDisplay.scrollTop = messageDisplay.scrollHeight;}, 1)
       }
     },
     created() {
+      this.$options.sockets.onmessage = (data) => {
+        const message = JSON.parse(data.data);
+        this.messages.push(message['message']);
+        this.scrollToBottom();
+      }
     }
-    // methods: {
-    //   pingServer() {
-    //     this.$socket.emit('', 'my message');
-    //   }
-    // }
   }
 </script>
 
 <style lang="scss" scoped>
-
+.chat-area {
+  margin: auto;
+  align-content: center;
+  min-height: 200px;
+  max-height: 200px;
+  overflow: scroll;
+  overflow-x: hidden;
+  min-width: 300px;
+  max-width: 50px;
+}
+input {
+  margin-right: 5px;
+  border-radius: 3px;
+  padding: 3px;
+  outline: none;
+  border: 1px solid #dddddd;
+}
 </style>
