c199221db91fe05de8b31e0c38b080074034a0df
Brian Caffey <briancaffey2010@gmail.com>
2019-04-24 23:00:33 -0400

    fixed channels consumer code for async websocketconsumer

--- backend/core/routing.py
+++ backend/core/routing.py
@@ -1,7 +1,7 @@
 from django.conf.urls import url
 
-from .consumers import CoreConsumer
+from . import consumers
 
 websocket_urlpatterns = [
-    url(r'^ws/chat/(?P<room_name>[^/]+)/$', CoreConsumer),
+    url(r'^ws/chat/(?P<room_name>[^/]+)/$', consumers.CoreConsumer),
 ]
