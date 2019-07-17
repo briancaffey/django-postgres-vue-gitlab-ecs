25303c30794972ff43b5589456b8170308f7e94f
Brian Caffey <briancaffey2010@gmail.com>
2019-07-17 00:34:20 -0400

    updated quasar config file

--- quasar/quasarfrontend/quasar.conf.js
+++ quasar/quasarfrontend/quasar.conf.js
@@ -1,60 +1,54 @@
 // Configuration for your app
 // https://quasar.dev/quasar-cli/quasar-conf-js
 
-module.exports = function (ctx) {
+module.exports = function(ctx) {
   return {
     // app boot file (/src/boot)
     // --> boot files are part of "main.js"
-    boot: [
-      'i18n',
-      'axios'
-    ],
-
-    css: [
-      'app.styl'
-    ],
+    boot: ["i18n", "axios"],
+    css: ["app.styl"],
 
     extras: [
-      // 'ionicons-v4',
-      // 'mdi-v3',
-      // 'fontawesome-v5',
-      // 'eva-icons',
-      // 'themify',
-      // 'roboto-font-latin-ext', // this or either 'roboto-font', NEVER both!
+      // "ionicons-v4",
+      // "mdi-v3",
+      // "fontawesome-v5",
+      // "eva-icons",
+      // "themify",
+      // "roboto-font-latin-ext", // this or either "roboto-font", NEVER both!
 
-      'roboto-font', // optional, you are not bound to it
-      'material-icons' // optional, you are not bound to it
+      "roboto-font", // optional, you are not bound to it
+      "material-icons" // optional, you are not bound to it
     ],
 
     framework: {
-      // iconSet: 'ionicons-v4',
-      // lang: 'de', // Quasar language
+      // iconSet: "ionicons-v4",
+      // lang: "de", // Quasar language
 
-      // all: true, // --- includes everything; for dev only!
+      all: true, // --- includes everything; for dev only!
 
       components: [
-        'QLayout',
-        'QHeader',
-        'QDrawer',
-        'QPageContainer',
-        'QPage',
-        'QToolbar',
-        'QToolbarTitle',
-        'QBtn',
-        'QIcon',
-        'QList',
-        'QItem',
-        'QItemSection',
-        'QItemLabel'
+        "QLayout",
+        "QHeader",
+        "QDrawer",
+        "QPageContainer",
+        "QPage",
+        "QToolbar",
+        "QToolbarTitle",
+        "QBtn",
+        "QIcon",
+        "QList",
+        "QItem",
+        "QItemSection",
+        "QItemLabel"
       ],
 
       directives: [
-        'Ripple'
+        "Ripple"
       ],
 
       // Quasar plugins
       plugins: [
-        'Notify'
+        "Notify"
       ]
     },
 
@@ -62,19 +56,19 @@ module.exports = function (ctx) {
 
     build: {
       scopeHoisting: true,
-      vueRouterMode: 'history',
+      vueRouterMode: "history",
       // vueCompiler: true,
       // gzip: true,
       // analyze: true,
       // extractCSS: false,
       extendWebpack (cfg) {
         cfg.module.rules.push({
-          enforce: 'pre',
+          enforce: "pre",
           test: /\.(js|vue)$/,
-          loader: 'eslint-loader',
+          loader: "eslint-loader",
           exclude: /node_modules/,
           options: {
-            formatter: require('eslint').CLIEngine.getFormatter('stylish')
+            formatter: require("eslint").CLIEngine.getFormatter("stylish")
           }
         })
       }
@@ -86,7 +80,7 @@ module.exports = function (ctx) {
       open: true // opens browser window automatically
     },
 
-    // animations: 'all', // --- includes all animations
+    // animations: "all", // --- includes all animations
     animations: [],
 
     ssr: {
@@ -94,53 +88,53 @@ module.exports = function (ctx) {
     },
 
     pwa: {
-      // workboxPluginMode: 'InjectManifest',
+      // workboxPluginMode: "InjectManifest",
       // workboxOptions: {}, // only for NON InjectManifest
       manifest: {
-        // name: 'Verbose Equals True',
-        // short_name: 'Verbose Equals True',
-        // description: 'Quasar frontend for Verbose Equals True',
-        display: 'standalone',
-        orientation: 'portrait',
-        background_color: '#ffffff',
-        theme_color: '#027be3',
+        // name: "Verbose Equals True",
+        // short_name: "Verbose Equals True",
+        // description: "Quasar frontend for Verbose Equals True",
+        display: "standalone",
+        orientation: "portrait",
+        background_color: "#ffffff",
+        theme_color: "#027be3",
         icons: [
           {
-            'src': 'statics/icons/icon-128x128.png',
-            'sizes': '128x128',
-            'type': 'image/png'
+            "src": "statics/icons/icon-128x128.png",
+            "sizes": "128x128",
+            "type": "image/png"
           },
           {
-            'src': 'statics/icons/icon-192x192.png',
-            'sizes': '192x192',
-            'type': 'image/png'
+            "src": "statics/icons/icon-192x192.png",
+            "sizes": "192x192",
+            "type": "image/png"
           },
           {
-            'src': 'statics/icons/icon-256x256.png',
-            'sizes': '256x256',
-            'type': 'image/png'
+            "src": "statics/icons/icon-256x256.png",
+            "sizes": "256x256",
+            "type": "image/png"
           },
           {
-            'src': 'statics/icons/icon-384x384.png',
-            'sizes': '384x384',
-            'type': 'image/png'
+            "src": "statics/icons/icon-384x384.png",
+            "sizes": "384x384",
+            "type": "image/png"
           },
           {
-            'src': 'statics/icons/icon-512x512.png',
-            'sizes': '512x512',
-            'type': 'image/png'
+            "src": "statics/icons/icon-512x512.png",
+            "sizes": "512x512",
+            "type": "image/png"
           }
         ]
       }
     },
 
     cordova: {
-      // id: 'org.cordova.quasar.app',
+      // id: "org.cordova.quasar.app",
       // noIosLegacyBuildFlag: true, // uncomment only if you know what you are doing
     },
 
     electron: {
-      // bundler: 'builder', // or 'packager'
+      // bundler: "builder", // or "packager"
 
       extendWebpack (cfg) {
         // do something with Electron main process Webpack cfg
@@ -151,10 +145,10 @@ module.exports = function (ctx) {
         // https://github.com/electron-userland/electron-packager/blob/master/docs/api.md#options
 
         // OS X / Mac App Store
-        // appBundleId: '',
-        // appCategoryType: '',
-        // osxSign: '',
-        // protocol: 'myapp://path',
+        // appBundleId: "",
+        // appCategoryType: "",
+        // osxSign: "",
+        // protocol: "myapp://path",
 
         // Windows only
         // win32metadata: { ... }
@@ -163,7 +157,7 @@ module.exports = function (ctx) {
       builder: {
         // https://www.electron.build/configuration/configuration
 
-        // appId: 'quasarfrontend'
+        // appId: "quasarfrontend"
       }
     }
   }
