951ef48d14da5a3579c6a8f11d72cadb51c4ca7f
Brian Caffey <briancaffey2010@gmail.com>
2019-07-16 23:48:07 -0400

    wip added quasar framework as alternative frontend

---
+++ quasar/quasarfrontend/src/pages/About.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+      <q-item>
+        Welcome to Verbose Equals True
+      </q-item>
+  </div>
+</template>
+
+<script>
+  export default {
+
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ quasar/quasarfrontend/src/pages/Index.vue
@@ -0,0 +1,13 @@
+<template>
+  <q-page class="flex flex-center">
+    <img alt="Quasar logo" src="~assets/quasar-logo-full.svg" />
+  </q-page>
+</template>
+
+<style></style>
+
+<script>
+export default {
+  name: "PageIndex"
+};
+</script>
