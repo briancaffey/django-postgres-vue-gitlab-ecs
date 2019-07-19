5ec1f163633816691644e36d8c01af499304541c
Brian Caffey <briancaffey2010@gmail.com>
2019-07-18 23:50:33 -0400

    added q-form to login modal

--- quasar/quasarfrontend/src/components/LoginModal.vue
+++ quasar/quasarfrontend/src/components/LoginModal.vue
@@ -1,31 +1,21 @@
 <template>
   <div>
-    <q-dialog
-      @hide="hideLoginMenu"
-      v-model="visible">
+    <q-dialog @hide="hideLoginMenu" v-model="visible">
       <q-card style="min-width: 400px">
         <q-card-section>
           <div class="text-h6">Login</div>
         </q-card-section>
-        <q-card-section>
-          <q-input v-model="username" type="text" label="Email" autofocus />
-          <q-input type="password" v-model="password" label="Password" />
-        </q-card-section>
+        <q-form @submit="login">
+          <q-card-section>
+            <q-input v-model="username" type="text" label="Email" autofocus />
+            <q-input type="password" v-model="password" label="Password" />
+          </q-card-section>
 
-        <q-card-actions align="right" class="text-primary">
-          <q-btn
-            tabindex="-1"
-            flat
-            label="Cancel"
-            v-close-popup
-          />
-          <q-btn
-            flat
-            label="Login"
-            @click="login()"
-            v-close-popup
-          />
-        </q-card-actions>
+          <q-card-actions align="right" class="text-primary">
+            <q-btn tabindex="-1" flat label="Cancel" v-close-popup />
+            <q-btn flat label="Login" type="submit" v-close-popup />
+          </q-card-actions>
+        </q-form>
       </q-card>
     </q-dialog>
   </div>
