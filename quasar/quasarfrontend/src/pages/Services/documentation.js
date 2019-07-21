4f89ba8627c763551743e055c1d44aeb6bd628da
Brian Caffey <bcaffey@drydengroup.com>
2019-07-20 20:15:50 -0400

    added documentation links

---
+++ quasar/quasarfrontend/src/pages/Services/documentation.js
@@ -0,0 +1,34 @@
+const documentationLinks = [
+  {
+    name: "Django",
+    href: "https://docs.djangoproject.com/en/2.2/",
+    type: "a",
+    target: "_blank"
+  },
+  {
+    name: "Vue.js",
+    href: "https://vuejs.org/v2/guide/",
+    type: "a",
+    target: "_blank"
+  },
+  {
+    name: "Docker",
+    href: "https://docs.docker.com/",
+    type: "a",
+    target: "_blank"
+  },
+  {
+    name: "GitLab",
+    href: "https://docs.gitlab.com/",
+    type: "a",
+    target: "_blank"
+  },
+  {
+    name: "ECS",
+    href: "https://docs.aws.amazon.com/ecs/index.html",
+    type: "a",
+    target: "_blank"
+  },
+]
+
+export default documentationLinks;
--- quasar/quasarfrontend/src/pages/Services/index.vue
+++ quasar/quasarfrontend/src/pages/Services/index.vue
@@ -13,15 +13,30 @@
         :label="s.name"
       />
     </div>
+    <h4>Documentation Links</h4>
+    <div class="q-pa-md q-gutter-sm">
+      <q-btn
+        v-for="(s, i) in documentationLinks"
+        :key="i"
+        :type="s.type"
+        :target="s.target"
+        :href="s.href"
+        color="primary"
+        :icon="s.icon || null"
+        :label="s.name"
+      />
+    </div>
   </base-page>
 </template>
 
 <script>
 import services from "./services.js";
+import documentationLinks from "./documentation.js";
 export default {
   data() {
     return {
-      services
+      services,
+      documentationLinks
     };
   }
 };
