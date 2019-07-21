89ea5b55ae2435e56207534dc7ec63ad93b06474
Brian Caffey <bcaffey@drydengroup.com>
2019-07-20 20:39:59 -0400

    added helpful links

--- quasar/quasarfrontend/src/pages/Services/index.vue
+++ quasar/quasarfrontend/src/pages/Services/index.vue
@@ -26,17 +26,32 @@
         :label="s.name"
       />
     </div>
+    <h4>Helpful Links</h4>
+    <div class="q-pa-md q-gutter-sm">
+      <q-btn
+        v-for="(s, i) in helpfulLinks"
+        :key="i"
+        :type="s.type"
+        :target="s.target"
+        :href="s.href"
+        color="primary"
+        :icon="s.icon || null"
+        :label="s.name"
+      />
+    </div>
   </base-page>
 </template>
 
 <script>
 import services from "./services.js";
 import documentationLinks from "./documentation.js";
+import helpfulLinks from "./helpfulLinks.js";
 export default {
   data() {
     return {
       services,
-      documentationLinks
+      documentationLinks,
+      helpfulLinks
     };
   }
 };
