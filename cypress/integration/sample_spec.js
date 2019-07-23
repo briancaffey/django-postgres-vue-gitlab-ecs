aa457d000645c6965d1365f185a37fa44f6f436f
Brian Caffey <briancaffey2010@gmail.com>
2019-07-22 22:23:41 -0400

    added command to service and added port to backend url for cypress

--- cypress/integration/sample_spec.js
+++ cypress/integration/sample_spec.js
@@ -1,5 +1,5 @@
 describe('Visit test endpoint', function() {
   it('Visits test endpoint', function() {
-    cy.visit('http://backend/api/hello-world')
+    cy.visit('http://backend:8000/api/hello-world')
   })
 })
