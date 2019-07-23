1d727ee62c1080c0ab68248a572f7cfa0a87cab3
Brian Caffey <briancaffey2010@gmail.com>
2019-07-22 22:56:46 -0400

    adjusted cypress debug test to log output

--- cypress/integration/sample_spec.js
+++ cypress/integration/sample_spec.js
@@ -1,5 +1,7 @@
 describe('Visit test endpoint', function() {
   it('Visits test endpoint', function() {
-    cy.visit('http://backend:8000/api/hello-world')
+    const resp = cy.request('http://backend:8000/api/hello-world').its('body');
+    console.log("response from debug api:");
+    console.log(resp);
   })
 })
