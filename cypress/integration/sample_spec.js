781bdeaf7af31b93fe1a2eaed2d73f208bda84fc
Brian Caffey <briancaffey2010@gmail.com>
2019-07-23 17:43:41 -0400

    debugging gitlab CI

--- cypress/integration/sample_spec.js
+++ cypress/integration/sample_spec.js
@@ -1,7 +1,7 @@
 describe('Visit test endpoint', function() {
   it('Visits test endpoint', function() {
     cy.log("visiting nginx host");
-    const resp = cy.visit('http://nginxci').its('body');
+    const resp = cy.visit('http://nginxci:80/about');
     cy.log("response from debug api:");
     cy.log(resp);
   })
