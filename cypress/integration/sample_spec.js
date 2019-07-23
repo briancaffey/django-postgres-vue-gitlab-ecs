3f1224e5128278f884aeecbf62d31056c9b082f9
Brian Caffey <briancaffey2010@gmail.com>
2019-07-23 18:50:37 -0400

    testing gitlab-ci cypress tests with base nginx image

--- cypress/integration/sample_spec.js
+++ cypress/integration/sample_spec.js
@@ -1,8 +1,9 @@
 describe('Visit test endpoint', function() {
   it('Visits test endpoint', function() {
     cy.log("visiting nginx host");
-    const resp = cy.visit('http://nginxci:80/about');
+    const resp = cy.visit('http://nginx').its('body');
     cy.log("response from debug api:");
     cy.log(resp);
+    expect('A').to.not.equal('B')
   })
 });
