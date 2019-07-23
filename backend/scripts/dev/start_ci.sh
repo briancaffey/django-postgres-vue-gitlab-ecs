10854cf168d482dc0abdaab10ce9fdbd70c7f1b3
Brian Caffey <briancaffey2010@gmail.com>
2019-07-23 18:56:39 -0400

    cat etc hosts and change cy.visit call

--- backend/scripts/dev/start_ci.sh
+++ backend/scripts/dev/start_ci.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 python3 manage.py collectstatic --no-input
+cat /etc/hosts
 python3 manage.py makemigrations
 python3 manage.py migrate --no-input
 gunicorn -t 300 -b 0.0.0.0:8000 backend.wsgi
--- cypress/integration/sample_spec.js
+++ cypress/integration/sample_spec.js
@@ -1,7 +1,7 @@
 describe('Visit test endpoint', function() {
   it('Visits test endpoint', function() {
     cy.log("visiting nginx host");
-    const resp = cy.visit('http://nginx').its('body');
+    const resp = cy.visit('http://nginx');
     cy.log("response from debug api:");
     cy.log(resp);
     expect('A').to.not.equal('B')
