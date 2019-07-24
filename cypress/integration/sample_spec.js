bc20bbeb503a519ad1038e228fc706ac0ca6d15b
Brian Caffey <briancaffey2010@gmail.com>
2019-07-24 17:51:59 -0400

    fixed local cypress e2e testing with gitlab runner

--- cypress/integration/sample_spec.js
+++ cypress/integration/sample_spec.js
@@ -1,10 +1,5 @@
-describe('Visit test endpoint', function() {
-  it('Visits test endpoint', function() {
-    cy.log("visiting nginx host");
-    cy.request("http://backend:8000/api/hello-world");
-    expect('A').to.not.equal('B');
+describe('Visit homepage and verify title', function() {
+  it('Finds the text of the homepage', function() {
     cy.visit("/");
-    cy.log("response from debug api:");
-    cy.log(resp);
   })
 });
