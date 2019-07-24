e5fae64cc2c238b114b3f1f924fd615b888e3175
Brian Caffey <briancaffey2010@gmail.com>
2019-07-24 00:39:40 -0400

    testing cypress test script

--- cypress/integration/sample_spec.js
+++ cypress/integration/sample_spec.js
@@ -1,9 +1,10 @@
 describe('Visit test endpoint', function() {
   it('Visits test endpoint', function() {
     cy.log("visiting nginx host");
-    const resp = cy.visit("/");
+    cy.request("http://backend:8000/api/hello-world");
+    expect('A').to.not.equal('B');
+    cy.visit("/");
     cy.log("response from debug api:");
     cy.log(resp);
-    expect('A').to.not.equal('B');
   })
 });
