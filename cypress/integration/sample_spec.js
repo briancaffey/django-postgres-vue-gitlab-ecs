8901323d8b5352316789b06f5e86a5d77fa1d764
Brian Caffey <briancaffey2010@gmail.com>
2019-07-24 00:20:42 -0400

    set base url in command line config

--- cypress/integration/sample_spec.js
+++ cypress/integration/sample_spec.js
@@ -1,9 +1,9 @@
 describe('Visit test endpoint', function() {
   it('Visits test endpoint', function() {
     cy.log("visiting nginx host");
-    const resp = cy.visit('http://nginx');
+    const resp = cy.visit(Cypress.env('FRONTEND_URL'));
     cy.log("response from debug api:");
     cy.log(resp);
-    expect('A').to.not.equal('B')
+    expect('A').to.not.equal('B');
   })
 });
