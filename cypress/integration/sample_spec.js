52ca4c8cf823f13e07ebae4b5c43f4ef3a03ea5d
Brian Caffey <briancaffey2010@gmail.com>
2019-07-24 00:26:13 -0400

    removed FRONTEND_URL env var

--- cypress/integration/sample_spec.js
+++ cypress/integration/sample_spec.js
@@ -1,7 +1,7 @@
 describe('Visit test endpoint', function() {
   it('Visits test endpoint', function() {
     cy.log("visiting nginx host");
-    const resp = cy.visit(Cypress.env('FRONTEND_URL'));
+    const resp = cy.visit("/");
     cy.log("response from debug api:");
     cy.log(resp);
     expect('A').to.not.equal('B');
