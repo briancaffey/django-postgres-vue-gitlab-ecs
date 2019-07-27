669971c95aec6c3bce86593b589a284a4302cb40
Brian Caffey <bcaffey@drydengroup.com>
2019-07-27 19:22:35 -0400

    added additional -f flag to cypress command in ci

--- integration-tests.sh
+++ integration-tests.sh
@@ -11,7 +11,7 @@ echo "Services are up and ready"
 echo "Seeding database with user"
 docker-compose -f docker-compose.ci.yml exec backend python manage.py create_default_user
 
-docker-compose -f cypress.yml up --exit-code-from cypress --build
+docker-compose -f docker-compose.ci.yml -f cypress.yml up --exit-code-from cypress --build
 
 echo "Cypress tests passed successfully."
 
