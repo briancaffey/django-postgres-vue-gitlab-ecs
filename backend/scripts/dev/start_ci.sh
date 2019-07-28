014d0f91180821c87bbf48b2bfa359d7823e87ed
Brian Caffey <bcaffey@drydengroup.com>
2019-07-28 00:37:00 -0400

    removed waits from start_ci

--- backend/scripts/dev/start_ci.sh
+++ backend/scripts/dev/start_ci.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
-ping postgres -c 20
 python3 manage.py collectstatic --no-input
-sleep 20
-cat /etc/hosts
 python3 manage.py makemigrations
 python3 manage.py migrate --no-input
 python3 manage.py create_default_user
