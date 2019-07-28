37c4b81038337926aa6bf310cef291712ecadf26
Brian Caffey <bcaffey@drydengroup.com>
2019-07-27 20:00:39 -0400

    fixed typo in django command call

--- backend/scripts/dev/start_ci.sh
+++ backend/scripts/dev/start_ci.sh
@@ -4,5 +4,5 @@ python3 manage.py collectstatic --no-input
 cat /etc/hosts
 python3 manage.py makemigrations
 python3 manage.py migrate --no-input
-python3 manage.py create_defaut_user
+python3 manage.py create_default_user
 gunicorn -t 300 -b 0.0.0.0:8000 backend.wsgi
