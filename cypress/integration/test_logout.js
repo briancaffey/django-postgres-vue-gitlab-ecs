e5d4183d7cb5d800bd553adbbf09225b98b28082
Brian Caffey <bcaffey@drydengroup.com>
2019-07-28 01:46:54 -0400

    removed nested cypress folder, added extra cookie check to lougout test

--- cypress/integration/test_logout.js
+++ cypress/integration/test_logout.js
@@ -2,6 +2,8 @@ describe('Test Login', function() {
   it("User can logout after having logged in.", function() {
     cy.login();
     cy.visit("/");
+    cy.getCookies()
+      .should('have.length', 2);
     cy.get("#logout").click();
     cy.getCookies()
     .should('have.length', 0)
