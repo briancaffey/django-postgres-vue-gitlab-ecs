76e162d52b8ddb4aa205eb20d1ba2e9edcf80a3d
Brian Caffey <briancaffey2010@gmail.com>
2019-07-28 22:51:11 -0400

    corrected name of cypress test

--- cypress/integration/test_homepage.js
+++ cypress/integration/test_homepage.js
@@ -1,5 +1,5 @@
-describe('Test Login', function() {
-  it("Can visit homepage", function() {
+describe('Test visit homepage', function() {
+  it("can visit the homepage", function() {
     cy.visit("/");
     cy.contains("Verbose Equals True");
   });
