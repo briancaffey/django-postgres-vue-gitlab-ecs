d7a45b8a587fa39e6dcaffa9ea901fa14af79329
Brian Caffey <bcaffey@drydengroup.com>
2019-08-03 17:12:29 -0400

    added tabs to login modal

--- quasar/src/components/LoginModal.vue
+++ quasar/src/components/LoginModal.vue
@@ -2,39 +2,61 @@
   <div>
     <q-dialog @hide="hideLoginMenu" v-model="visible">
       <q-card style=" max-width: 95%; min-width: 320px;">
-        <q-card-section>
-          <div class="text-h6">Login</div>
-        </q-card-section>
-        <q-form @submit="login">
-          <q-card-section>
-            <q-input
-              id="email"
-              v-model="email"
-              type="text"
-              label="Email"
-              autofocus
-            />
-            <q-input
-              id="password"
-              type="password"
-              v-model="password"
-              label="Password"
-            />
-          </q-card-section>
+        <q-tabs
+          v-model="tab"
+          dense
+          align="justify"
+          class="bg-primary text-white shadow-2"
+          :breakpoint="0"
+        >
+          <q-tab name="login" icon="vpn_key">Login</q-tab>
+          <q-tab name="signup" icon="account_circle">Sign Up</q-tab>
+        </q-tabs>
+        <q-tab-panels v-model="tab" animated>
+          <q-tab-panel name="login">
+            <q-form @submit="login">
+              <q-input
+                id="email"
+                v-model="email"
+                type="text"
+                label="Email"
+                autofocus
+              />
+              <q-input
+                id="password"
+                type="password"
+                v-model="password"
+                label="Password"
+              />
 
-          <q-card-actions align="right" class="text-primary">
-            <q-btn tabindex="-1" flat label="Cancel" v-close-popup />
+              <q-card-actions align="right" class="text-primary">
+                <q-btn tabindex="-1" flat label="Cancel" v-close-popup />
+                <q-btn
+                  id="login-btn"
+                  flat
+                  label="Login"
+                  type="submit"
+                  v-close-popup
+                />
+              </q-card-actions>
+              <q-btn type="a" class="full-width q-mt-md" :href="githuboath2link"
+                >GitHub</q-btn
+              >
+              <br />
+            </q-form>
+          </q-tab-panel>
+
+          <q-tab-panel name="signup">
+            <div class="text-h6">TODO</div>
+            Implement Sign up with Email + Email Verification
             <q-btn
-              id="login-btn"
-              flat
-              label="Login"
-              type="submit"
-              v-close-popup
+              :href="githuboath2link"
+              type="a"
+              class="full-width q-mt-md"
+              label="Sign Up with GitHub"
             />
-          </q-card-actions>
-          <a class="q-btn" :href="githuboath2link">GitHub</a>
-          <br />
-        </q-form>
+          </q-tab-panel>
+        </q-tab-panels>
       </q-card>
     </q-dialog>
   </div>
@@ -46,6 +68,7 @@ import buildURL from "axios/lib/helpers/buildURL";
 export default {
   data() {
     return {
+      tab: "login",
       params: {
         client_id: "d6639d522598d6bf20f4",
         redirect_uri: "http://localhost/auth/github/callback",
