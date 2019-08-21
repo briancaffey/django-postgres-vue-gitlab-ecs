b7ed458f9689e95d547f65babb62cb0446c5ce26
Brian Caffey <briancaffey2010@gmail.com>
2019-08-20 20:34:00 -0400

    added job guards, fixed pages job

--- documentation/docs/.vuepress/config.js
+++ documentation/docs/.vuepress/config.js
@@ -2,7 +2,7 @@ module.exports = {
   title: "Verbose Equals True",
   base: "/django-postgres-vue-gitlab-ecs/",
   port: 8080,
-  dest: "public",
+  dest: "../public",
   plugins: {
     "@vuepress/google-analytics": {
       ga: "UA-131443776-1"
