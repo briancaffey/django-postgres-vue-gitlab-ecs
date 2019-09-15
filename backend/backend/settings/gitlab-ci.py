1a8a0fd154f425eed4330ba171cdeab901ef4fdb
Brian Caffey <briancaffey2010@gmail.com>
2019-09-15 15:40:30 -0400

    removed old middleware

--- backend/backend/settings/gitlab-ci.py
+++ backend/backend/settings/gitlab-ci.py
@@ -30,7 +30,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'core.middleware.SubdomainMiddleware',
 ]
 
 
