74a2d2dd30f199dc95d1a005818ae13fdbfef784
Brian Caffey <bcaffey@drydengroup.com>
2019-11-11 08:42:55 -0500

    added basic redis usage to websocket example

--- quasar/src/pages/Examples/Websockets.vue
+++ quasar/src/pages/Examples/Websockets.vue
@@ -3,15 +3,26 @@
     <page-header>Websockets example</page-header>
     <page-sub-header>Ping Pong: {{ wsUrl }}</page-sub-header>
     <base-btn id="ping" @click.native="sendPing">Send Ping</base-btn>
-
-    <q-chip
-      color="teal"
-      text-color="white"
-      class="pong"
-      v-for="(pong, i) in pongs"
-      :key="i"
-      >PONG</q-chip
-    >
+    <br /><br />
+    <div class="cards">
+      <base-card
+        dark
+        color="teal"
+        text-color="white"
+        class="pong"
+        v-for="(pong, i) in pongs"
+        :key="i"
+      >
+        Cached Value: {{ pong["cached_value"] }}<br />
+        Vue Ping: 0 ({{ pong["vue_ping"] }}) <br />
+        Server Recv: +{{ pong["server_recv_ping"] - pong["vue_ping"] }} ms<br />
+        Server Send Pong: +{{
+          pong["server_send_pong"] - pong["vue_ping"]
+        }}
+        ms<br />
+        Vue Recv Pong: +{{ pong["vue_recv_pong"] - pong["vue_ping"] }} ms
+      </base-card>
+    </div>
   </base-page>
 </template>
 
@@ -20,13 +31,17 @@ export default {
   data() {
     return {
       wsUrl: process.env.WS_PING_PONG,
-      pongs: []
+      pongs: [],
+      showLatency: false
     };
   },
   created() {
     this.$connect(this.wsUrl, { format: "json" });
-    this.$socket.onmessage = () => {
-      this.pongs.push("PONG");
+    this.$socket.onmessage = i => {
+      console.log(i);
+      const data = JSON.parse(i["data"]);
+      data.vue_recv_pong = new Date().getTime();
+      this.pongs.unshift(data);
     };
   },
   methods: {
@@ -34,7 +49,8 @@ export default {
       this.$socket.send(
         JSON.stringify({
           message: "ping",
-          sender: 1
+          sender: 1,
+          ts: new Date().getTime()
         })
       );
     }
@@ -45,4 +61,14 @@ export default {
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style scoped>
+.cards {
+  display: grid;
+  grid-gap: 10px;
+  grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
+}
+
+.pong {
+  padding: 10px;
+}
+</style>
