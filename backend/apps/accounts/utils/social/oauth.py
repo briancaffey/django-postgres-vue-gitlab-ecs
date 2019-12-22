85f8e49b1d474b1a0edd0fe06bb318f8225e3143
Brian Caffey <bcaffey@drydengroup.com>
2019-12-22 08:35:12 -0500

    added todo note [skip ci]

--- backend/apps/accounts/utils/social/oauth.py
+++ backend/apps/accounts/utils/social/oauth.py
@@ -22,6 +22,7 @@ def get_payload(backend, code):
             'client_secret': client_secret,
         }
 
+    # TODO: change `localhost` to env var
     elif backend == 'google-oauth2':
         payload = {
             'code': code,
