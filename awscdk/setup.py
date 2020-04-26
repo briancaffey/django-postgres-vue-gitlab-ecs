92a8814b641699651ceccf5b2aef6f5e30361c03
Brian <briancaffey2010@gmail.com>
2020-04-26 16:32:37 -0400

    updated aws cdk dependencies

--- awscdk/setup.py
+++ awscdk/setup.py
@@ -8,51 +8,40 @@ with open("README.md") as fp:
 setuptools.setup(
     name="awscdk",
     version="0.0.1",
-
-    description="An empty CDK Python app",
+    description="Verbose Equals True application",
     long_description=long_description,
     long_description_content_type="text/markdown",
-
-    author="author",
-
+    author="Brian Caffey",
     package_dir={"": "awscdk"},
     packages=setuptools.find_packages(where="awscdk"),
-
     install_requires=[
-        "aws-cdk.core==1.25.0",
-        "aws-cdk.aws_certificatemanager==1.25.0",
-        "aws-cdk.aws_route53==1.25.0",
-        "aws-cdk.aws_s3==1.25.0",
-        "aws-cdk.aws_cloudfront==1.25.0",
-        "aws-cdk.aws_route53_targets==1.25.0",
-        "aws-cdk.aws_ecr==1.25.0",
-        "aws-cdk.aws_ec2==1.25.0",
-        "aws-cdk.aws_rds==1.25.0",
-        "aws-cdk.aws_ssm==1.25.0",
-        "aws-cdk.aws_elasticache==1.25.0",
-        "aws-cdk.aws_elasticloadbalancingv2==1.25.0",
-        "aws-cdk.aws_ecs==1.25.0",
-        "aws-cdk.aws_autoscaling==1.25.0"
+        "aws-cdk.core==1.35.0",
+        "aws-cdk.aws_certificatemanager==1.35.0",
+        "aws-cdk.aws_route53==1.35.0",
+        "aws-cdk.aws_s3==1.35.0",
+        "aws-cdk.aws_cloudfront==1.35.0",
+        "aws-cdk.aws_route53_targets==1.35.0",
+        "aws-cdk.aws_ecr==1.35.0",
+        "aws-cdk.aws_ec2==1.35.0",
+        "aws-cdk.aws_rds==1.35.0",
+        "aws-cdk.aws_ssm==1.35.0",
+        "aws-cdk.aws_elasticache==1.35.0",
+        "aws-cdk.aws_elasticloadbalancingv2==1.35.0",
+        "aws-cdk.aws_ecs==1.35.0",
+        "aws-cdk.aws_autoscaling==1.35.0",
     ],
-
     python_requires=">=3.6",
-
     classifiers=[
         "Development Status :: 4 - Beta",
-
         "Intended Audience :: Developers",
-
         "License :: OSI Approved :: Apache Software License",
-
         "Programming Language :: JavaScript",
         "Programming Language :: Python :: 3 :: Only",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
-
         "Topic :: Software Development :: Code Generators",
         "Topic :: Utilities",
-
         "Typing :: Typed",
     ],
 )
