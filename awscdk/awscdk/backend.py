d2cb18ef7ead4c681331caef931f4bbd3eddac9e
Brian <briancaffey2010@gmail.com>
2020-05-04 02:43:02 -0400

    added health check, removed pandas from base dependencies

--- awscdk/awscdk/backend.py
+++ awscdk/awscdk/backend.py
@@ -62,4 +62,9 @@ class Backend(core.Construct):
             targets=[self.backend_service],
             priority=1,
             path_patterns=["*"],
+            health_check=elbv2.HealthCheck(
+                healthy_http_codes="200-299",
+                path="/api/hello-world",
+                port="443",
+            ),
         )
