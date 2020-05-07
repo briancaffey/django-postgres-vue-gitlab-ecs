2bdb21ae9092c1cc3ac758b1d25f7346fa4e09dd
Brian <briancaffey2010@gmail.com>
2020-05-07 00:02:28 -0400

    debugging health check by changing port

--- awscdk/awscdk/backend.py
+++ awscdk/awscdk/backend.py
@@ -58,7 +58,7 @@ class Backend(core.Construct):
 
         load_balancer.https_listener.add_targets(
             "BackendTarget",
-            port=80,
+            port=8000,
             targets=[self.backend_service],
             priority=1,
             path_patterns=["*"],
