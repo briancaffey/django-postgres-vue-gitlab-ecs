4c210b6b4e8714b7517949c3439fdeba458ed3b2
Brian <briancaffey2010@gmail.com>
2020-05-10 22:39:22 -0400

    added create superuser command

--- awscdk/awscdk/backend_tasks.py
+++ awscdk/awscdk/backend_tasks.py
@@ -1,3 +1,5 @@
+import os
+
 from aws_cdk import (
     core,
     aws_ecs as ecs,
@@ -47,5 +49,28 @@ class BackendTasks(core.Construct):
             command=["python3", "manage.py", "collectstatic", "--no-input"],
             logging=ecs.LogDrivers.aws_logs(
                 stream_prefix="CollectstaticCommand"
+            ),
+        )
+
+        self.create_superuser_task = ecs.FargateTaskDefinition(
+            self,
+            "CreateSuperuserTask",
+            family=f"{full_app_name}-create-superuser",
+        )
+
+        self.create_superuser_task.add_container(
+            "CreateSuperuserCommand",
+            image=image,
+            environment=environment_variables.regular_variables,
+            secrets=environment_variables.secret_variables.update(
+                {
+                    "SUPERUSER_PASSWORD": os.environ.get(
+                        "SUPERUSER_PASSWORD", "password"
+                    )
+                }
+            ),
+            command=["python3", "manage.py", "create_default_user"],
+            logging=ecs.LogDrivers.aws_logs(
+                stream_prefix="CreateSuperuserCommand"
             ),
         )
