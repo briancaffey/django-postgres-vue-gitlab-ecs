43fd7d40d2aec4c33b618934ee94401e17059d2d
Brian <briancaffey2010@gmail.com>
2020-05-29 22:49:05 -0400

    give task permission to put CloudWatch metric data

--- awscdk/awscdk/celery_default.py
+++ awscdk/awscdk/celery_default.py
@@ -7,7 +7,7 @@ from aws_cdk import (
     aws_ecs_patterns as ecs_patterns,
     aws_logs as logs,
     aws_cloudformation as cloudformation,
-    aws_cloudwatch as cloudwatch,
+    aws_cloudwatch as cw,
     aws_applicationautoscaling as aas,
 )
 
@@ -68,7 +68,7 @@ class CeleryDefaultServiceStack(cloudformation.NestedStack):
                 self.celery_default_worker_service.task_definition.task_role
             )
 
-        self.default_celery_queue_cloudwatch_metric = cloudwatch.Metric(
+        self.default_celery_queue_cw_metric = cw.Metric(
             namespace=scope.full_app_name, metric_name="default"
         )
 
@@ -78,7 +78,7 @@ class CeleryDefaultServiceStack(cloudformation.NestedStack):
 
         self.celery_default_queue_asg.scale_on_metric(
             "CeleryDefaultQueueAutoscaling",
-            metric=self.default_celery_queue_cloudwatch_metric,
+            metric=self.default_celery_queue_cw_metric,
             scaling_steps=[
                 aas.ScalingInterval(change=1, lower=0),
                 aas.ScalingInterval(change=-1, lower=1),
@@ -86,11 +86,11 @@ class CeleryDefaultServiceStack(cloudformation.NestedStack):
             adjustment_type=aas.AdjustmentType.CHANGE_IN_CAPACITY,
         )
 
-        self.celery_default_cloudwatch_monitor_task = ecs.FargateTaskDefinition(
-            self, "CeleryDefaultCloudWatchMonitoringTask"
+        self.celery_default_cw_monitor_task = ecs.FargateTaskDefinition(
+            self, "CeleryDefaultCWMonitoringTask"
         )
 
-        self.celery_default_cloudwatch_monitor_task.add_container(
+        self.celery_default_cw_monitor_task.add_container(
             "CeleryDefaultCWMonitoringTaskContainer",
             image=scope.image,
             logging=ecs.LogDrivers.aws_logs(
@@ -109,7 +109,7 @@ class CeleryDefaultServiceStack(cloudformation.NestedStack):
             targets=[
                 events_targets.EcsTask(
                     cluster=scope.cluster,
-                    task_definition=self.celery_default_cloudwatch_monitor_task,
+                    task_definition=self.celery_default_cw_monitor_task,
                     subnet_selection=ec2.SubnetSelection(
                         subnet_type=ec2.SubnetType.PUBLIC
                     ),
@@ -122,14 +122,6 @@ class CeleryDefaultServiceStack(cloudformation.NestedStack):
             ],
         )
 
-        # self.celery_default_cloudwatch_monitoring_task.add_container(
-        #     "CeleryDefaultCloudWatchMonitoringContainer",
-        #     image=scope.image,
-        #     logging=ecs.LogDrivers.aws_logs(
-        #         stream_prefix="CeleryDefaultCloudWatchMonitoringContainerLogs",
-        #         log_retention=logs.RetentionDays.ONE_DAY,
-        #     ),
-        #     environment=scope.variables.regular_variables,
-        #     secrets=scope.variables.secret_variables,
-        #     command=["python3", "manage.py", "put_celery_cloudwatch_metrics"],
-        # )
+        self.default_celery_queue_cw_metric.grant_put_metric_data(
+            self.celery_default_cw_monitor_task.task_role
+        )
