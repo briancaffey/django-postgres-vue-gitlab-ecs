a56543467535b278d9bb0b4885910e1aa182e93f
Brian <briancaffey2010@gmail.com>
2020-06-06 09:24:54 -0400

    fixed private media storage in loocal development

--- backend/backend/storage_backends.py
+++ backend/backend/storage_backends.py
@@ -1,4 +1,5 @@
 from django.conf import settings
+from django.core.files.storage import FileSystemStorage
 from storages.backends.s3boto3 import S3Boto3Storage
 
 
@@ -6,8 +7,16 @@ class StaticStorage(S3Boto3Storage):
     location = settings.AWS_STATIC_LOCATION
 
 
-class PrivateMediaStorage(S3Boto3Storage):
-    location = settings.AWS_PRIVATE_MEDIA_LOCATION
-    default_acl = "private"
-    file_overwrite = False
-    custom_domain = False
+if settings.DEBUG:
+
+    class PrivateMediaStorage(FileSystemStorage):
+        location = settings.AWS_PRIVATE_MEDIA_LOCATION
+
+
+else:
+
+    class PrivateMediaStorage(S3Boto3Storage):
+        location = settings.AWS_PRIVATE_MEDIA_LOCATION
+        default_acl = 'private'
+        file_overwrite = False
+        custom_domain = False
